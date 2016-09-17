c78c23c2b27bb42f1ac3101954eefe2045f72f65
mckloveschrist10 <Mckenzie.parent@gmail.com>
2016-09-17 13:12:35 -0400

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class hello
+{
+
+   public static void main(String args[])
+   {
+   
+     Scanner in = new Scanner(System.in);
+      System.out.print("My very first program on GitHub, how interesting\n");
+      System.out.printf("%s", "So what is your name, and dont say Github");
+      String personNam = in.nextLine();
+      System.out.println("Nice to meet you, have a good day");
+      
+      }
+      
+      }
